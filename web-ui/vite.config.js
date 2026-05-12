480ee21bcfaf5afb73937c0fe4a5f97f27803071
Rasmus <rasmus@triggerz.net>
2026-05-12 15:11:27 +0200

    allow host

--- web-ui/vite.config.js
+++ web-ui/vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: ['all'],
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3000',
